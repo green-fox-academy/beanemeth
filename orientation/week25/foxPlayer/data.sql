f487e82e07c41b4f10a0344039884462ab64c3d1
beanemeth <bea.nemeth@yahoo.co.uk>
2022-05-30 11:39:26 +0200

    adding foxPlayer backend

--- orientation/week25/foxPlayer/data.sql
+++ orientation/week25/foxPlayer/data.sql
@@ -12,27 +12,46 @@ CREATE TABLE playlists (
   PRIMARY KEY (id)
 );
 
-DROP TABLE IF  EXISTS tracks;  
+DROP TABLE IF  EXISTS tracks; 
 CREATE TABLE tracks (
     id INT AUTO_INCREMENT,
     title VARCHAR(120) NOT NULL,
     artist VARCHAR(120) NOT NULL,
     duration INT,
-    path TINYTEXT NOT NULL,
-    playlist_id INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (playlist_id) REFERENCES playlists(id), 
+    path TINYTEXT,
+    PRIMARY KEY (id)
+    -- FOREIGN KEY (playlist_id) REFERENCES playlists(id), 
 );
 
+CREATE TABLE playlists_tracks (
+  playlists_id INT NOT NULL,
+  tracks_id INT NOT NULL,
+  PRIMARY KEY (playlists_id, tracks_id),
+  FOREIGN KEY (playlists_id) REFERENCES playlists(id),
+  FOREIGN KEY (tracks_id) REFERENCES tracks(id)
+);
 
 INSERT INTO playlists (playlist_title, system_rank)
 VALUES
     ("Favorites", 1),
-    ("Fox house", 0),
+    ("Fox rock", 0),
     ("Driving", 0),
     ("Music for programming", 0);
-  
 
 
+INSERT INTO tracks (title, artist, duration )
+VALUES
+    ("Novemver rain", "Guns and Roses", 180),
+    ("Penny Lane", "The beatles", 190),
+    ("Under the Sun", "DIIV", 195),
+    ("Lightning Bolt", "Jake Bugg", 190);
+
+INSERT INTO playlists_tracks (playlists_id, tracks_id)
+VALUES
+    (1, 1),
+    (2, 2),
+    (3, 3);
+
+ --many to many, coz one track can belong to more than one playlist
 
 
--- orientation/week25/foxPlayer/server.js
+++ orientation/week25/foxPlayer/server.js
@@ -32,7 +32,7 @@ conn.connect((err) => {
 //Return playlists in the order they were added
 
 app.get('/api/playlists', (req, res) => {
-    const query = `SELECT * FROM playlists`;  //order by id?
+    const query = `SELECT * FROM playlists`;
     conn.query(query, (err, rows) => {
         if (err) {
             console.error(err);
@@ -47,15 +47,13 @@ app.get('/api/playlists', (req, res) => {
 
 // Creates a new playlist
 // The only required field is a title property that contains the name of the playlist
-// System playlists cannot be created by the client !!!!!!!!!!!!!!!!!!!
+// System playlists cannot be created by the client !!!!!!!!!!!!!!!!!!! favourites, 
 
 app.post('/api/playlists', (req, res) => {
     if (!req.body.playlist_title) {
         res.status(400).send('Missing playlist title');
         return;
     }
-
-
     const query1 = `SELECT * FROM playlists WHERE playlist_title = ?`;
     const params = [req.body.playlist_title];
 
@@ -97,35 +95,133 @@ app.post('/api/playlists', (req, res) => {
 // if the id parameter is not present or not a number, return an error in JSON format. You might explain the error briefly.
 // If the system_rank field is set to 1, you mustn't delete the playlist
 
-app.delete('/playlists/:id', (req, res) => {
+app.delete('/api/playlists/:id', (req, res) => {
     const id = parseInt(req.params.id);
 
     if (!id || isNaN(id)) {
         res.status(404).send({ message: 'Not found' });
         return;
     }
-    if (parseInt(req.params.system_rank) == 1) {
-        res.status(400).send('Sorry cannot delete, as system rank is 1');
-        return;
-    }
+
     const query2 = 'SELECT * FROM playlists WHERE id = ?';
-    conn.query(query2, [id], (err2) => {
+    conn.query(query2, [id], (err2, result) => {
         if (err2) {
             console.error(err2);
-            res.status(500).send({ message: err2.sqlMessage }); //server oldali hiba
+            res.status(500).send({ message: err2.sqlMessage });
             return;
         }
-        res.status(204).send('Deleted playlist');
 
+        if (!result[0]) {
+            res.status(404).send('Sorry cannot delete, id does not match');
+            return;
+        }
+        if (parseInt(result[0].system_rank) == 1) {
+            res.status(400).send('Sorry cannot delete, as system rank is 1');
+            return;
+        }
         const query = `DELETE FROM playlists WHERE id = ?`;
         conn.query(query, [id], (error) => {
             if (error) {
                 res.status(500).send({ message: 'Error, unable to delete' });
                 return;
             }
+            res.status(204).send('Deleted playlist');
         });
     });
 });
+
+// GET /playlist-tracks/
+
+// Without a playlist_id it should return all tracks in the root folder. Subfolders should be explored recursively.
+app.get('/api/playlist-tracks', (req, res) => {
+    const query = `SELECT * FROM tracks`;
+    conn.query(query, (err, rows) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send(err.sqlMessage);
+            return;
+        }
+        res.send(rows);
+    });
+});
+
+// GET /playlist-tracks/:playlist_id
+
+// Lists all the tracks added to the playlist, left outer join: kilistazza azokat is 
+//amik nincsenek egy playlistben sem, inner join: csak azokat amik bennevannak vlmelyik playlistben
+
+app.get('/api/playlist-tracks/:playlist_id', (req, res) => {
+        const query = `
+            SELECT * FROM tracks
+            INNER JOIN playlists_tracks
+            ON tracks.id = playlists_tracks.tracks_id
+            WHERE playlists_tracks.playlists_id = ?
+        `;
+        const params = [parseInt(req.params.playlist_id)];
+    
+    console.log(params)
+    console.log(req.params)
+    //console.log(req.params.id) // eloszor veletlen play;ist id helyett siman id-t irtam es akkor NAN lett 
+    //[ NaN ]
+    // { playlist_id: '1' }
+    // undefined
+
+        conn.query(query, params, (err, rows) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send({ message: err.sqlMessage });
+                return;
+            }
+            if (rows.length === 0) {
+                res.status(404).send({ message: 'Not found' });
+                return;
+            }
+            res.send(rows[0]);
+        });
+    });
+    
+//     POST /playlist-tracks/:playlist_id
+
+// Adds the track to the playlist identified by playlist_id
+
+app.post('/api/playlist-tracks/:playlist_id', (req, res) => {
+
+    const query = `
+            INSERT INTO playlists_tracks (playlists_id, tracks_id)
+            VALUES (?, ?)
+        `;
+        const params = [parseInt(req.params.playlist_id), req.body.tracks_id];
+
+        conn.query(query, params, (err2) => {
+            if (err2) {
+                console.error(err2);
+                res.status(500).send({ message: err2.sqlMessage });
+                return;
+            }
+
+            res.status(201).send({ message: 'Success' });
+        });
+    });
+
+    // DELETE /playlist-tracks/:playlist_id/:track_id
+
+    // Deletes the track with track_id from the playlist with playlist_id
+    // If one of the id parameters is not present or not a number, return an error in JSON format. 
+    // You might explain the error briefly.
+    app.delete('/api/playlist-tracks/:playlist_id/:track_id', (req, res) => {
+
+        const query1 = `DELETE FROM playlists_tracks WHERE playlists_id = ? AND tracks_id = ? `;
+        const params = [parseInt(req.params.playlist_id), parseInt(req.params.track_id)];
+    
+        conn.query(query1, params, (err1) => {
+            if (err1) {
+                console.error(err1);
+                res.status(500).send({ message: err1.sqlMessage });
+                return;
+            }
+            res.status(204).send('Deleted playlist');
+        });
+    });
 
 
 
